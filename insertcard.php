08a2b2a0b4a9a6541281d83d1fa8503d022d0fc6
vn35 <33454992+vn35@users.noreply.github.com>
2017-11-09 16:45:08 +0700

    Add files via upload

---
+++ insertcard.php
@@ -0,0 +1,22 @@
+<?php 
+session_start();
+require_once 'listproduc.php';
+$idProduct = $_GET['id'];
+$newProduct=  array();
+foreach ($product as $key => $val) {
+	$newProduct[$val['id']] = $val;
+}
+ 
+ 
+if (!isset($_SESSION['cart']) || $_SESSION['cart'] == null) {
+	$newProduct[$idProduct]['qty'] = 1;
+	$_SESSION['cart'][$idProduct] = $newProduct[$idProduct];
+} else {
+	if (array_keys_exists($idProduct, $_SESSION['cart'])) {
+		$_SESSION['cart'][$idProduct]['qty'] +=1;
+	} else {
+		$newProduct[$idProduct]['qty'] = 1;
+		$_SESSION['cart'][$idProduct] = $newProduct[$idProduct];
+	}
+}
+header("location:giohang.php"); 
